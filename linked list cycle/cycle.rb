893cf02438c4fffc46cf612144187b40b7fa0143
Jon Wexler <jwexman@gmail.com>
2016-01-11 00:10:55 -0500

    cycling nodes

---
+++ linked list cycle/cycle.rb
@@ -0,0 +1,78 @@
+# Jon Wexler
+# Check if a singly linked list has a cycle
+
+# A cycle: when a node in the linked list's next value points to a node already passed in the list
+# Write a function contains_cycle() that takes the first node in a singly-linked list and returns a boolean indicating whether the list contains a cycle.
+
+# Using the following as a node:
+class LinkedListNode
+    attr_accessor :data, :next
+
+    def initialize value
+        @data = value
+        @next = nil
+    end
+end
+
+a = LinkedListNode.new(5)
+a.next = LinkedListNode.new(4)
+a.next.next = LinkedListNode.new(3)
+a.next.next.next = LinkedListNode.new(2)
+a.next.next.next.next = LinkedListNode.new(1)
+a.next.next.next.next.next = LinkedListNode.new(0)
+a.next.next.next.next.next.next = a
+
+# O(1) space, O(n) time #1 solution
+def contains_node3 node
+
+    first = second = node
+
+    while second != nil && second.next != nil
+        # puts "HERE #{second}"
+
+        first = first.next
+        second = second.next.next
+
+        if first == second
+            return true
+        end
+    end
+
+    return false
+end
+
+puts contains_node3 a
+
+# O(n) space, and O(n) time
+def contains_node node
+    nodes = []
+    while node.next
+        nodes.push node
+        if nodes.include?(node.next)
+            return true
+        end
+        node = node.next
+    end
+    return false
+end
+puts contains_node a
+
+# O(1) space, and O(n) time, but destructive
+# duplicate node is only a shallow copy and still affects following methods
+def contains_node2 node
+    # node = node.clone
+    while node.next 
+        if node.next == 3
+            return true
+        end
+        temp = node.next
+        node.next = 3
+        node = temp
+    end
+
+    return false
+end
+
+puts contains_node2 a
+
+
--- stock trading/trading.rb
+++ stock trading/trading.rb
@@ -1,23 +1,29 @@
 # Jon Wexler
 # This code takes an array and outputs the maximum difference between two values
 # as long as the the minimum value occurs before the maximum value
+# O(n) time and O(nlogn) space
 
+# max_difference takes an array and recursively finds the minimum and maximum values in the array to calculate the greatest difference
 def max_difference(a)
 
-    mx = a.max
-    mn = a.min
+# max and min values are taken from array a
+mx = a.max
+mn = a.min
 
-    if a.index(mx)>=a.index(mn)
-        return mx-mn;
-    end
+# If the min number appears before the max then we have the greatest difference
+if a.index(mx)>=a.index(mn)
+    return mx-mn;
+end
 
-    b = a.dup
-    c = a.dup
+# duplicate array a twice
+b = a.dup
+c = a.dup
 
-    b.delete(mx)
-    c.delete(mn)
 
-    return [max_proft(b), max_proft(c)].max
+b.delete(mx)
+c.delete(mn)
+
+return [max_proft(b), max_proft(c)].max
 end
 
 a = [4,3,56,4,12,32,-3,32,0,65,3,12,-4,34,23]
